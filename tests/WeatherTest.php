7744079e391d90f020801e076efd51fbc569d63d
Adrià Manero <adriamanero97@gmail.com>
2022-11-06 12:11:10 +0100

    fix: tests

--- tests/WeatherTest.php
+++ tests/WeatherTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\WeatherKata;
 
-use WeatherKata\Forecast;
+use WeatherKata\GetForecastPredictionController;
 use PHPUnit\Framework\TestCase;
 
 class WeatherTest extends TestCase
@@ -10,7 +10,7 @@ class WeatherTest extends TestCase
     /** @test */
     public function find_the_weather_of_today()
     {
-        $forecast = new Forecast();
+        $forecast = new GetForecastPredictionController();
         $city     = "Madrid";
 
         $prediction = $forecast->predict($city);
@@ -21,18 +21,18 @@ class WeatherTest extends TestCase
     /** @test */
     public function find_the_weather_of_any_day()
     {
-        $forecast = new Forecast();
+        $forecast = new GetForecastPredictionController();
         $city     = "Madrid";
 
         $prediction = $forecast->predict($city, new \DateTime('+2 days'));
 
-        $this->assertEquals('sunny', $prediction );
+        $this->assertEquals('sunny', $prediction);
     }
 
-/** @test */
+    /** @test */
     public function find_the_wind_of_any_day()
     {
-        $forecast = new Forecast();
+        $forecast = new GetForecastPredictionController();
         $city = "Madrid";
 
         $prediction = $forecast->predict($city, null, true);
@@ -43,7 +43,7 @@ class WeatherTest extends TestCase
     /** @test */
     public function change_the_city_to_woeid()
     {
-        $forecast = new Forecast();
+        $forecast = new GetForecastPredictionController();
         $city = "Madrid";
 
         $forecast->predict($city, null, true);
@@ -54,7 +54,7 @@ class WeatherTest extends TestCase
     /** @test */
     public function there_is_no_prediction_for_more_than_5_days()
     {
-        $forecast = new Forecast();
+        $forecast = new GetForecastPredictionController();
         $city = "Madrid";
 
         $prediction = $forecast->predict($city, new \DateTime('+6 days'));
